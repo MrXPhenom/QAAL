c11e09afe5c8213511c3014f46f28b913f336f7a
MrXPhenom <oleksandr.kalyushko92@gmail.com>
2022-11-07 03:38:41 +0200

    commit

---
+++ src/homework/HW1.java
@@ -0,0 +1,25 @@
+package homework;
+
+public class HW1 {
+    public static void main(String []arg) {
+        int a = 10;
+        int b = 20;
+        int c = 100;
+        int t = c;
+        String myNameInTelegram = "Alter Ego";
+        System.out.print("a = "
+                + a
+                + "\n"
+                + "b = "
+                + b
+                + "\n"
+                + "c = "
+                + c
+                + "\n"
+                + "t = "
+                + t
+                + "\n"
+                + "My name in Telegram: "
+                + myNameInTelegram);
+    }
+}
---
+++ src/homework/HW2.java
@@ -0,0 +1,25 @@
+package homework;
+
+public class HW2 {
+    public static void main(String []arg) {
+        int number = 3;
+        int x = 10;
+        int y = 30;
+        int z = 5;
+        System.out.println( "Task "
+                +
+                number
+                +
+                "\n"
+                +
+                x
+                +
+                "\n"
+                +
+                y
+                +
+                "\n"
+                +
+                z);
+    }
+}
