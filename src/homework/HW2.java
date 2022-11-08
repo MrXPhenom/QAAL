450d5e3bca4f2f5f132a5d9150905f5c8a1308fc
MrXPhenom <oleksandr.kalyushko92@gmail.com>
2022-11-08 14:53:49 +0200

    commit

--- src/homework/HW2.java
+++ src/homework/HW2.java
@@ -6,19 +6,34 @@ public class HW2 {
         int x = 10;
         int y = 30;
         int z = 5;
+        String a = "a ";
+        String b = "b ";
+        String c = "c ";
         System.out.println( "Task "
                 +
                 number
                 +
                 "\n"
                 +
+                number
+                +
+                a
+                +
                 x
                 +
                 "\n"
                 +
+                number
+                +
+                b
+                +
                 y
                 +
                 "\n"
+                +
+                number
+                +
+                c
                 +
                 z);
     }
